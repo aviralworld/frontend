6c39f1be5154995211e3af6b1a6ebcdd3a5f3551
Shiv Jha-Mathur <4199779-shivjm@users.noreply.gitlab.com>
2020-10-14 20:20:26 +0530

    feat(server.ts): export new `SETTINGS` variable

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,16 @@ import * as sapper from "@sapper/server";
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
-polka() // You can also use Express
+polka()
   .use(
     compression({ threshold: 0 }),
     sirv("static", { dev }),
     sapper.middleware(),
   )
-  .listen(PORT, (err) => {
+  .listen(PORT, (err: any) => {
     if (err) console.log("error", err);
   });
+
+export const SETTINGS = {
+  apiUrl: new URL(process.env.API_URL),
+};
