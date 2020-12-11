5d9d135e0bb8dcd9dd8577c5b7642ded023d388f
Shiv Jha-Mathur <4199779-shivjm@users.noreply.gitlab.com>
2020-12-11 19:03:25 +0530

    feat(Dropdown.svelte): add placeholder when field is required

--- src/components/Dropdown.svelte
+++ src/components/Dropdown.svelte
@@ -10,6 +10,8 @@
 <select id={id} name={name} required={!optional} bind:value>
   {#if optional}
     <option value={undefined}>Prefer not to say</option>
+  {:else}
+    <option value={undefined} disabled selected hidden>Select one…</option>
   {/if}
   {#each options as [value, label] (value)}
     <option value={value}>{label}</option>
