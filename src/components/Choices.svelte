9ff1348f8b05357fdfaf929daba08421e48346fa
Shiv Jha-Mathur <4199779-shivjm@users.noreply.gitlab.com>
2020-12-13 02:03:19 +0530

    fix!: replace `Dropdown` with `Choices`

---
+++ src/components/Choices.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let name;
+  export let options;
+  export let optional;
+
+  export let selection = undefined;
+</script>
+
+<style>
+  fieldset { border: 0; }
+
+  label {
+    display: block;
+  }
+
+  label + label {
+    margin-top: 1rem;
+  }
+</style>
+
+<fieldset>
+  {#if optional}
+    <label><input type="radio" name={name} bind:group={selection} value={undefined}> Prefer not to say</label>
+    {/if}
+  {#each options as [value, label] (value)}
+    <label><input type="radio" name={name} bind:group={selection} value={value} /> {label}</label>
+    {/each}
+</fieldset>
--- src/components/Metadata.svelte
+++ src/components/Metadata.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import Dropdown from "./Dropdown.svelte";
+  import Choices from "./Choices.svelte";
 
   export let ages;
   export let categories;
@@ -47,16 +47,16 @@
 <label for="user-name">What is your name? <input type="text" name="name" id="user-name" bind:value={name} required /></label>
 
 <!-- TODO use radio buttons with descriptions -->
-<p>What is your story about? <Dropdown options={categories} id="user-category" name="category" optional={false} bind:selection={category_id} /></p>
+<p>What is your story about? <Choices options={categories} id="user-category" name="category" optional={false} bind:selection={category_id} /></p>
 
 <section>
   <p class="note">The following fields are optional. They will only be used for research purposes and will never be shared publicly.</p>
 
   <label for="user-occupation">What is your occupation? <input type="text" name="occupation" id="user-occupation" bind:value={occupation} /></label>
 
-  <p>What is your age? <Dropdown options={ages} id="user-age" name="age" optional={true} bind:selection={age_id} /></p>
+  <p>What is your age? <Choices options={ages} id="user-age" name="age" optional={true} bind:selection={age_id} /></p>
 
-  <p>What gender do you identify as? <Dropdown options={genders} id="user-gender" name="gender" optional={true} bind:selection={gender_id} /></p>
+  <p>What gender do you identify as? <Choices options={genders} id="user-gender" name="gender" optional={true} bind:selection={gender_id} /></p>
 
   <label for="user-location">Where do you live? <input type="text" name="location" id="user-location" bind:value={location} /></label>
 </section>
