d3f0888343bb8ed4a8efe3281ee1368efda22b13
Shiv Jha-Mathur <4199779-shivjm@users.noreply.gitlab.com>
2020-12-25 20:28:43 +0530

    fix(reply form): remove whitespace when checking validity of name

--- src/components/Record.svelte
+++ src/components/Record.svelte
@@ -11,8 +11,11 @@
   let recorder = writable(undefined);
   let currentTime: number;
 
+  let rawName;
   export let name;
   export let categoryId;
+
+  $: name = rawName === undefined ? "" : rawName.trim();
 
   // these are provided by the parent component
   export let formats;
@@ -121,8 +124,8 @@
     <form class="record">
       <p>First, please let us know a few details:</p>
 
-      <RequiredMetadata bind:name bind:categoryId categories={categories} />
-      <button on:click|preventDefault={handleRecording} class="button record-button" disabled={name === undefined || name.trim() === "" || categoryId === undefined}>
+      <RequiredMetadata bind:name={rawName} bind:categoryId categories={categories} />
+      <button on:click|preventDefault={handleRecording} class="button record-button" disabled={name === "" || categoryId === undefined}>
         {#if inProgress}
           Stop recording ({asMinutesAndSeconds(currentTime)}/{asMinutesAndSeconds(maxRecordingLengthSeconds)})
         {:else}
