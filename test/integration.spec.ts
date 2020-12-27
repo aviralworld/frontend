0fbbaa5ccf5e20c16782e213208304592a0690e2
Shiv Jha-Mathur <4199779-shivjm@users.noreply.gitlab.com>
2020-12-27 18:06:38 +0530

    test: run server from tests

--- test/integration.spec.ts
+++ test/integration.spec.ts
@@ -1,3 +1,4 @@
+import { fork } from "child_process";
 import { readFileSync } from "fs";
 
 import dotenvSafe from "dotenv-safe";
@@ -8,18 +9,60 @@ import puppeteer from "puppeteer";
 
 dotenvSafe.config();
 
-const { PORT } = process.env;
-const {
-  TESTING_AVW_HOST,
-  PG_CONNECTION_STRING,
-  SHOW_PUPPETEER_BROWSER,
-} = process.env;
+const { PG_CONNECTION_STRING, SHOW_PUPPETEER_BROWSER } = process.env;
 
-const BASE = new URL(`${TESTING_AVW_HOST}`);
 const SEED = 0.7;
+
+const START_PORT = 50000;
+const END_PORT = 55000;
+
+function randomInt(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const serverPort = randomInt(START_PORT, END_PORT);
+const apiPort = serverPort + 1;
 
 describe("The server", function () {
   this.timeout(process.env.MOCHA_TIMEOUT);
+
+  const server = fork("./__sapper__/build", [], {
+    env: {
+      PORT: serverPort.toString(),
+      FRONTEND_API_URL: `http://127.0.0.1:3030/`,
+      ROARR_LOG: "true",
+      DEBUG: "express-http-proxy",
+    },
+    stdio: "pipe",
+  });
+
+  server.on("error", (e) => {
+    throw new Error(e.toString());
+  });
+
+  const exitListener = () => {
+    throw new Error("child process exited");
+  };
+
+  server.on("exit", exitListener);
+
+  let baseUrl: URL;
+
+  const urlPromise = new Promise((resolve) => {
+    server.stdout.on("data", (line: Buffer) => {
+      try {
+        const string = line.toString();
+        const parsed = JSON.parse(string);
+        const { port } = parsed.context;
+        if (port !== undefined) {
+          baseUrl = new URL(`http://127.0.0.1:${port}/`);
+          resolve();
+          return;
+        }
+      } catch (e) {}
+    });
+  });
+
   let browser: puppeteer.Browser;
 
   before(async () => {
@@ -117,6 +160,8 @@ describe("The server", function () {
     await client.query("SELECT setseed($1);", [SEED]);
 
     await client.end();
+
+    await urlPromise;
   });
 
   before(async () => {
@@ -128,19 +173,22 @@ describe("The server", function () {
   after(async () => {
     try {
       await browser.close();
+
+      server.off("exit", exitListener);
+      server.kill();
     } catch (e) {}
   });
 
   it("can be reached", async () => {
     const page = await browser.newPage();
-    await page.goto(BASE.toString());
+    await page.goto(baseUrl.toString());
     assert.equal(await page.title(), "A Viral World");
   });
 
   it("returns 404 for non-existent recordings", async () => {
     const page = await browser.newPage();
     const response = await page.goto(
-      new URL("/recording/youdontexist/", BASE).toString(),
+      new URL("/recording/youdontexist/", baseUrl).toString(),
     );
     assert.equal(response.status(), 404);
   });
@@ -150,7 +198,7 @@ describe("The server", function () {
     const response = await page.goto(
       new URL(
         "/recording/f11ecb2c-3225-4fa9-a059-0bd2601496e9/",
-        BASE,
+        baseUrl,
       ).toString(),
     );
     assert.equal(response.status(), 410);
@@ -161,10 +209,13 @@ describe("The server", function () {
     await page.goto(
       new URL(
         "/recording/14b68be1-08d8-4d74-afdf-1da226b821ff/",
-        BASE,
+        baseUrl,
       ).toString(),
     );
 
-    assert.equal(await page.title(), "A story by Avon Boliviano Cotton (cyan)");
+    assert.equal(
+      await page.title(),
+      "A story by Avon Boliviano Cotton from cyan",
+    );
   });
 });
