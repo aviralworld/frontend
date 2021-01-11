d90c6b661a834b012ae0c6cfdb4b159283f7c719
Shiv Jha-Mathur <4199779-shivjm@users.noreply.gitlab.com>
2021-01-11 17:32:18 +0530

    ui(Link): use readonly, copy-able text input instead of plain text

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -1,5 +1,46 @@
 <script lang="ts">
   export let href;
+
+  let input;
+  let copied = false;
+
+  async function copy() {
+    copied = false;
+    await navigator.clipboard.writeText(href);
+    copied = true;
+  }
 </script>
 
-<a href={href}><code>{href}</code></a>
+<style>
+  form {
+    margin-top: 0.5rem;
+    display: flex;
+    flex-flow: row;
+    width: 100%;
+  }
+
+  input, button {
+    padding: 0.5rem;
+  }
+
+  input {
+    font-family: monospace;
+    flex: 1 1 99%;
+  }
+
+  button {
+    flex: 1 0 1ch;
+    margin-left: -1px; /* hide the doubled border */
+    transition: color 0.5s;
+    color: rgba(0, 0, 0, 1);
+  }
+
+  .copied {
+    color: rgba(0, 0, 0, 0.75);
+  }
+</style>
+
+<form>
+  <input type="text" readonly value={href} on:click={() => input.setSelectionRange(0, input.value.length)} bind:this={input} />
+  <button on:click|preventDefault={copy} class:copied>{#if copied}Copied!{:else}Copy{/if}</button>
+</form>
