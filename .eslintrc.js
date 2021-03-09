35d87e87371963ba10cc0bcdf36e967f11fd7b2b
Shiv Jha-Mathur <4199779-shivjm@users.noreply.gitlab.com>
2021-03-09 17:45:49 +0530

    chore: delete unused ESLint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,15 +7,6 @@ module.exports = {
     extraFileExtensions: [".svelte"],
   },
   plugins: ["svelte3", "@typescript-eslint"],
-  rules: {
-    // copied from
-    // <https://dev.to/mhaecker/use-airbnb-s-eslint-config-with-typescript-prettier-in-svelte-apps-4fb7>,
-    // should review
-    // "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
-    // "import/no-mutable-exports": 0,
-    // "no-labels": 0,
-    // "no-restricted-syntax": 0,
-  },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
@@ -30,18 +21,10 @@ module.exports = {
     {
       files: ["**/*.svelte"],
       processor: "svelte3/svelte3",
-      rules: {
-        // according to the docs, these don't work well with Svelte
-        "import/first": "off",
-        "import/no-duplicates": "off",
-        "import/no-mutable-exports": "off",
-        "import/no-unresolved": "off",
-      },
       plugins: ["@typescript-eslint"],
     },
   ],
   settings: {
     "svelte3/typescript": require("typescript"),
-    // ...
   },
 };
