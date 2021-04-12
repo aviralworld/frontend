8ebe1310c072d8bd9de42cadcc85b58eb605dcae
Shiv Jha-Mathur <4199779-shivjm@users.noreply.gitlab.com>
2021-04-12 10:37:28 +0530

    fix(admin.ts): add types

--- src/admin.ts
+++ src/admin.ts
@@ -1,16 +1,22 @@
-import { Client } from "pg";
+import { Client, QueryResult } from "pg";
 
 const { PG_CONNECTION_STRING } = process.env;
 
 const QUERY = "SELECT parent_id, id FROM open_recordings LIMIT 1";
 
-export async function fetchOpenToken() {
+interface IResult {
+  id: string;
+  parent_id: string;
+}
+
+export async function fetchOpenToken(): Promise<IResult> {
   const client = new Client({ connectionString: PG_CONNECTION_STRING });
   await client.connect();
 
-  const { rows } = await client.query(QUERY);
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+  const { rows } = (await client.query(QUERY, [])) as QueryResult<IResult>;
 
-  const first = rows.shift();
+  const first: IResult = rows.shift();
 
   if (first === undefined) {
     throw new Error("No open recordings found");
