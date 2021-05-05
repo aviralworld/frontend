e872daf396d9420a42b260ce1a971c7273a64ff3
Shiv Jha-Mathur <4199779-shivjm@users.noreply.gitlab.com>
2021-05-05 18:07:30 +0530

    feat!: add details to health check endpoint and always return 200

--- src/server/healthCheck.ts
+++ src/server/healthCheck.ts
@@ -15,20 +15,29 @@ export default function healthCheck(
   };
 
   return async (_req, res) => {
-    let apiReachable = false;
-
     res.contentType(CONTENT_TYPE);
     res.status(200);
 
-    try {
-      apiReachable = await axios.get(apiEndpoint, {
-        timeout: apiHealthCheckTimeoutMs,
-      });
-      apiReachable = true;
-    } catch (e) {
-      res.status(504);
-    }
+    const [apiStatus, error] = await tryApi(
+      apiEndpoint,
+      apiHealthCheckTimeoutMs,
+    );
 
-    res.send(JSON.stringify({ ...info, apiReachable }));
+    res.send(JSON.stringify({ ...info, api: { status: apiStatus, error } }));
   };
+}
+
+async function tryApi(
+  endpoint: string,
+  timeout: number,
+): Promise<[number, any]> {
+  try {
+    const response = await axios.get(endpoint, {
+      timeout: timeout,
+    });
+
+    return [response.status, undefined];
+  } catch (e) {
+    return [e.reponse?.status, e];
+  }
 }
